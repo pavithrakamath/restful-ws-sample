91064c0df8c2a16fa589f81e820ab3d0fa9d359a
Pavithra Kamath <pavithrakamathgp@gmail.com>
2018-05-13 02:12:41 +0530

    using path variable

--- restful-ws/src/main/java/com/pk/rest/ws/controllers/HelloWorldController.java
+++ restful-ws/src/main/java/com/pk/rest/ws/controllers/HelloWorldController.java
@@ -1,14 +1,19 @@
 package com.pk.rest.ws.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
-	@GetMapping("/hello-world")
+	@GetMapping("/hello-world/{name}")
+	public String SayHelloWorld(@PathVariable String name) {
+		return "Hello world " + name + "!";
+	}
+
+	@GetMapping("/")
 	public String SayHelloWorld() {
 		return "Hello world";
 	}
-
 }
