8c8d172de5c6dcdc41add3fd3c8281953c4e05f3
Pavithra Kamath <pavithrakamathgp@gmail.com>
2018-05-13 22:03:03 +0530

    formatting

--- restful-ws/src/main/java/com/pk/rest/ws/controllers/UsersController.java
+++ restful-ws/src/main/java/com/pk/rest/ws/controllers/UsersController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -16,6 +17,7 @@ import com.pk.rest.ws.beans.User;
 import com.pk.rest.ws.services.UserService;
 
 @RestController
+@RequestMapping("/")
 public class UsersController {
 
 	@Autowired
